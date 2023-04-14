30b083c134e1e9f939aec2ca84a805960bff269b
thalesAlves758 <thalesalves758@gmail.com>
2023-04-13 21:30:32 -0300

    feat: create Tag class

---
+++ src/main/java/Tag.java
@@ -0,0 +1,17 @@
+public class Tag {
+    private String name;
+    private String color;
+
+    public Tag(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
